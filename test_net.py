761fa3dae84005c595d1ece039ce4d464865ade0
grq <ruoqian_guo@163.com>
2018-08-21 11:16:36 +0800

    finish

--- test_net.py
+++ test_net.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
         args.imdbval_name = "vg_150-50-50_minival"
         args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]']
 
-    args.cfg_file = "cfgs/{}.yml".format(args.net)
+    args.cfg_file = "cfgs/{}_ls.yml".format(args.net) if args.large_scale else "cfgs/{}.yml".format(args.net)
 
 
     if args.cfg_file is not None:
